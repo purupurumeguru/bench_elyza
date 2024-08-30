c7ebae94a1d6c57abc414d9dbd462fe659a67c02
purupurumeguru <purupuru3219@protonmail.com>
2024-08-30 10:06:38 -0500

    modify eval repos

--- run.py
+++ run.py
@@ -4,9 +4,11 @@ def run_models():
     judge_model = "gpt-4o-mini"
     seed = 314159265
     model_list = [
-        ("mradermacher/EZO-Common-9B-gemma-2-it-GGUF", "EZO-Common-9B-gemma-2-it.f16.gguf"),
-        ("mradermacher/Llama-3.1-8B-EZO-1.1-it-GGUF", "Llama-3.1-8B-EZO-1.1-it.f16.gguf"),
-        ("mradermacher/gemma-2-9B-it-advanced-v2.1-GGUF", "gemma-2-9B-it-advanced-v2.1.f16.gguf"),
+        ("bartowski/gemma-2-9b-it-GGUF", "gemma-2-9b-it-Q8_0.gguf"),
+        ("mradermacher/EZO-Common-9B-gemma-2-it-GGUF", "EZO-Common-9B-gemma-2-it.Q8_0.gguf"),
+        ("bartowski/Meta-Llama-3.1-8B-Instruct-GGUF", "Meta-Llama-3.1-8B-Instruct-Q8_0.gguf"),
+        ("mradermacher/Llama-3.1-8B-EZO-1.1-it-GGUF", "Llama-3.1-8B-EZO-1.1-it.Q8_0.gguf"),
+        ("mmnga/Llama-3-ELYZA-JP-8B-gguf", "Llama-3-ELYZA-JP-8B-Q8_0.gguf")
     ]
 
     for repo_id, filename in model_list:
